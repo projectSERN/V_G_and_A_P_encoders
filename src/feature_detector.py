c574086714a0d3788b8a02bbc761a59f451443cc
NuhaAz <nuhaaisyah9@gmail.com>
2025-03-24 14:18:15 +0000

    New facial feature detection file

---
+++ src/feature_detector.py
@@ -0,0 +1,274 @@
+import os
+import cv2
+import sys
+
+import torch
+from torchvision import transforms
+import numpy as np
+
+from src.rtgene.extract_landmarks_method_base import LandmarkMethodBase
+from src.rtgene.gaze_tools import visualize_landmarks
+
+# Using RT-Gene (SFD [faceboxes] + 3DDFA [facial landmarks]) to extract face and eye images 
+class extract_face_features():
+    """
+    Class to extract relevant face features.
+    
+    Licensing:
+    Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+    """
+    def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu"):
+        self.device = device
+        script_path = os.path.dirname(os.path.realpath(__file__))
+        sys.path.append(os.path.abspath(os.path.join(script_path, '../..')))
+        self.landmark_estimator = LandmarkMethodBase(device_id_facedetection=self.device,
+                                            checkpoint_path_face=os.path.abspath(os.path.join(script_path, "../src/rtgene/model_nets/SFD/s3fd_facedetector.pth")),
+                                            checkpoint_path_landmark=os.path.abspath(
+                                                os.path.join(script_path, "../src/rtgene/model_nets/phase1_wpdc_vdc.pth.tar")),
+                                            model_points_file=os.path.abspath(os.path.join(script_path, "../src/rtgene/model_nets/face_model_68.txt")))
+    
+    def __extract_eye_image_patches(self, subject):
+        """
+        Extract eye image patches from the subject
+        
+        Args:
+            subject: subject object containing facial landmarks
+        Returns:
+            None
+        """
+        le_c, re_c, _, _ = subject.get_eye_image_from_landmarks(subject, self.landmark_estimator.eye_image_size)
+        subject.left_eye_color = le_c
+        subject.right_eye_color = re_c
+        
+    def __calculate_iou(self, boxA, boxB):
+        """
+        Calculate the Intersection over Union (IoU) of two bounding boxes
+        
+        Args:
+            boxA: first bounding box
+            boxB: second bounding box
+        Returns:
+            IoU: Intersection over Union of the two bounding boxes
+        """
+        xA = max(boxA[0], boxB[0])
+        yA = max(boxA[1], boxB[1])
+        xB = min(boxA[2], boxB[2])
+        yB = min(boxA[3], boxB[3])
+
+        interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+        boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+        boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+
+        iou = interArea / float(boxAArea + boxBArea - interArea)
+        return iou
+        
+    def __match_faces(self, previous_subjects, current_subjects):
+        """
+        Match current faceboxes to previous faceboxes
+        
+        Args:
+            previous_subjects: dictionary of faceboxes from previous frame (key = subject index)
+            current_subjects: list of subjects from current frame
+        Returns:
+            matched_subjects: dictionary of matched faces to subject index => {'subject index': subject object}
+        """
+        matched_subjects = {}
+        for current_subject in current_subjects:
+            threshold = 0.6
+            matched = None
+            # for previous_box in previous_subjects:
+            for previous_subject in previous_subjects:
+                # Calculate IoU between current facebox and previous facebox
+                if previous_subjects[previous_subject] is None:
+                    continue
+                previous_box = previous_subjects[previous_subject].box
+                current_box = current_subject.box
+                iou = self.__calculate_iou(current_box, previous_box)
+                if iou > threshold:
+                    matched = previous_subject
+            matched_subjects[matched] = current_subject
+            
+        return matched_subjects
+    
+    def __visualize_face_tracking(self, frames, subjects):
+        """
+        Visualize face tracking in a video and save the video
+        
+        Args:
+            frames: list of video frames
+            subjects: list of subjects
+        Returns:
+            None
+        """
+        colours = {}
+        for subject in subjects:
+            new_colour = (int(np.random.randint(0, 255)), int(np.random.randint(0, 255)), int(np.random.randint(0, 255)))
+            colours[subject] = new_colour
+            
+        tracked_frames = []
+        for index in range(len(frames)):
+            img = frames[index].copy()
+            for subject in subjects:
+                if subjects[subject][index] is not None:
+                    box = subjects[subject][index].box.astype(int)
+                    img = cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), colours[subject], 2)
+            tracked_frames.append(img)
+        
+        out = cv2.VideoWriter('face_tracking.avi', cv2.VideoWriter_fourcc(*'DIVX'), 30, (frames[0].shape[1], frames[0].shape[0]))
+        for frame in tracked_frames:
+            out.write(frame)
+        out.release()
+    
+    def extract_subjects(self, frames):
+        """
+        Extract subjects from video frames.
+        
+        Args:
+            frames: list of the video frames
+        Returns:
+            subjects: dictionary of extracted faces, {'subject index': [subject object for each frame]}
+            error: the error associated with the video (default = None)
+        """
+        error = None
+        subjects = {}
+        previous_faceboxes = None
+        
+        current_frame = 0
+        for frame in frames:
+            faceboxes = self.landmark_estimator.get_face_bb(frame)
+            if len(faceboxes) == 0:
+                print('ERROR: Could not find faces in the image')
+                return None, "Face"
+            
+            current_subjects = self.landmark_estimator.get_subjects_from_faceboxes(frame, faceboxes)
+            
+            if previous_faceboxes is not None:
+                # Match faceboxes to previous faceboxes
+                matched_subjects = self.__match_faces(previous_faceboxes, current_subjects)
+                
+                # If previous subject not detected in current frame, append None to its current frame
+                for subject in subjects:
+                    if subject not in matched_subjects:
+                        matched_subjects[subject] = None       
+                        
+                for matched_subject in matched_subjects:
+                    if matched_subject not in subjects:
+                        # Add new dictionary key if new face detected
+                        new_subject_index = len(subjects)
+                        subjects[new_subject_index] = [None] * current_frame
+                        subjects[new_subject_index].append(matched_subjects[matched_subject])
+                    else:   
+                        # Append subject object depending on subject index (i.e. value of matched_subject)
+                        subjects[matched_subject].append(matched_subjects[matched_subject])
+            else:
+                for index in range(len(current_subjects)):
+                    subjects[index] = [current_subjects[index]]
+            
+            # Link previous faceboxes based on subject index
+            previous_faceboxes = {}
+            for subject in subjects:
+                previous_faceboxes[subject] = subjects[subject][current_frame]
+            current_frame += 1
+        
+        # Remove any subject tracked for less than one second
+        removed_subjects = []
+        for subject in subjects:
+            frames_tracked = len(subjects[subject]) - subjects[subject].count(None)
+            if frames_tracked != len(frames):
+                removed_subjects.append(subject)        
+        for subject in removed_subjects:
+            del subjects[subject]
+        
+        if len(subjects) > 1:
+            print(f"ERROR: More than one subject detected - {len(subjects)}")
+            error = "Subjects"
+        
+        # self.__visualize_face_tracking(frames, subjects)
+        
+        return subjects, error
+            
+    def extract_eye_images(self, subjects):
+        """
+        Extract left and right eyes from video frames.
+        
+        Args:
+            subjects: dictionary of subjects extracted from video frames
+        Returns:
+            eyes_left: dictionary of left eye images extracted from each subject for each frame
+            eyes_right: dictionary of right eye images extracted from each subject for each frame
+            error: error message if no eyes detected (default = None)
+        """
+        error = None
+        eyes_left = {}
+        eyes_right = {}
+        index = 0
+        # For each frame where face is extracted, extract left and right eyes
+        for index in subjects:
+            # Define subject in dictionary
+            eyes_left[index] = []
+            eyes_right[index] = []
+            
+            for subject in subjects[index]:
+                # Go through each frame for each subject
+                
+                # Extract eye patches
+                self.__extract_eye_image_patches(subject)
+                if subject.left_eye_color is None or subject.right_eye_color is None:
+                    # If no eyes detected, disregard video
+                    print('ERROR: Could not find eyes in the image')
+                    return None, None, "Eyes"
+                else:
+                    eyes_left[index].append(torch.from_numpy(subject.left_eye_color).float())
+                    eyes_right[index].append(torch.from_numpy(subject.right_eye_color).float())
+                    
+            # Processing of eye images
+            eyes_left[index] = np.array(eyes_left[index])
+            eyes_right[index] = np.array(eyes_right[index])
+                
+            eyes_left[index] = torch.from_numpy(eyes_left[index]).float().cuda(self.device)
+            eyes_right[index] = torch.from_numpy(eyes_right[index]).float().cuda(self.device)
+            
+            if eyes_left[index].dim() == 1 or eyes_right[index].dim() == 1:
+                # Need to investigate why error occurs
+                print("ERROR: Eye images are not correct shape")
+                return None, None, "Dimensions"
+            
+            eyes_left[index] = eyes_left[index].permute(0, 3, 1, 2)
+            eyes_right[index] = eyes_right[index].permute(0, 3, 1, 2)
+            
+            transformations = transforms.Compose([
+                    transforms.Normalize(mean=[0.2323, 0.2088, 0.1767],
+                                        std=[0.0621, 0.0577, 0.0613])
+                ])
+            
+            eyes_left[index] = transformations(eyes_left[index])
+            eyes_right[index] = transformations(eyes_right[index])
+        
+        return eyes_left, eyes_right, error
+    
+    def extract_lip_landmarks(self, subjects):
+        """
+        Extract lip landmarks from video frames.
+        
+        Args:
+            subjects: dictionary of subjects extracted from video frames
+        Returns:
+            lip_landmarks: list of lip landmarks extracted from each frame - attached to subject index
+        """
+        lip_landmarks = {}
+        for subject in subjects:
+            frames = subjects[subject]
+            lip_landmark = {'upperlip': [], 'lowerlip': []}
+            for frame in frames:
+                landmarks = frame.landmarks.astype(int)
+                # Standard index for upper inner lip landmarks (61-63)
+                upperlip_landmarks = landmarks[61:64]
+                lip_landmark['upperlip'].append(upperlip_landmarks)
+                
+                # Standard index for lower inner lip landmarks (65-67)
+                lowerlip_landmarks = landmarks[67:64:-1]
+                lip_landmark['lowerlip'].append(lowerlip_landmarks)
+                
+            lip_landmarks[subject] = lip_landmark
+ 
+        return lip_landmarks
--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,12 @@
 import os
 import cv2
-import sys
 
 import torch
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
-from torchvision import transforms
 
-from src.rtgene.extract_landmarks_method_base import LandmarkMethodBase
+from dataset import *
 
 def gazeto2d(gaze, rad=False):
     # Convert gaze vector to pitch and yaw angles
@@ -20,32 +18,51 @@ def gazeto2d(gaze, rad=False):
         gaze_2d = (gaze_2d) * (180/np.pi)
     return gaze_2d
 
+
 # EDIT FOR NEW NN MODEL
-def get_arnet_vars(model):
-    r_var = [model.arnetbase, model.arnetdense, model.arnetsplit]
-    for i in range(len(r_var)):
-        for module_name, module in r_var[i].named_modules():
-            for name, param in module.named_parameters():
-                yield param
+def get_vars(model):
+    for name, param in model.named_parameters():
+        yield param
 
-# EDIT FOR DFDC DATASET
-def load_data(path, batch_size, transformations, train=False, valid=False, test=False):
+
+def load_data(path, batch_size):
     # Load data for train, validation and test
-    if train:
-        dataset = RTGene(path, transformations, train=True)
-    elif valid:
-        dataset = RTGene(path, transformations, valid=True)
-    else:
-        dataset = RTGene(path, transformations, test=True)
+    dataset = DFDC_preprocessed(path)
+    
+    # Split dataset into train, validation, and test sets
+    train_size = int(0.8 * len(dataset))
+    val_size = int(0.1 * len(dataset))
+    test_size = len(dataset) - train_size - val_size
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+        dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(120)
+    )
 
-    data = DataLoader(
-        dataset=dataset,
+    train_loader = DataLoader(
+        dataset=train_dataset,
         batch_size=int(batch_size),
         shuffle=True,
         num_workers=0,
-        pin_memory=True)
+        pin_memory=True
+    )
     
-    return data
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=int(batch_size),
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True
+    )
+    
+    test_loader = DataLoader(
+        dataset=test_dataset,
+        batch_size=int(batch_size),
+        shuffle=False,
+        num_workers=0,
+        pin_memory=True
+    )
+
+    return train_loader, val_loader, test_loader
+
 
 def early_stopping(avg_val_loss, best_val_loss, epochs_no_improve, epoch, model, output):
     # Early stopping algorithm
@@ -59,6 +76,7 @@ def early_stopping(avg_val_loss, best_val_loss, epochs_no_improve, epoch, model,
         
     return best_val_loss, epochs_no_improve
 
+
 def loger(epoch, train_loss, val_loss, outfile, model, output):
     # Log the average loss for each epoch
     loger = f"[epoch---{epoch+1}] Train Loss:{train_loss}, Val Loss:{val_loss}\n"
@@ -69,12 +87,12 @@ def loger(epoch, train_loss, val_loss, outfile, model, output):
     torch.save(model.state_dict(), output +'/'+ '_epoch_' + str(epoch+1) + '.pth')
 
 
-def plot_loss(path, epochs, train_loss, val_loss, angle, log=False):
+def plot_loss(path, epochs, train_loss, val_loss, dataset, log=False):
     # Plot training and validation loss
     fig = plt.figure(figsize=(14, 8))        
-    plt.xlabel('epoch')
-    plt.ylabel(f'avg error ({angle})')
-    plt.title('Gaze angular error (MAE)')
+    plt.xlabel('Epoch')
+    plt.ylabel('Avg error (BCE Loss)')
+    plt.title(f'Binary Cross Entropy Loss - {dataset}')
     plt.plot(epochs, train_loss, color='tab:red', label='train')
     plt.plot(epochs, val_loss, color='tab:blue', label='val')
     plt.legend(['Train_Loss', 'Val_Loss'])
@@ -85,7 +103,8 @@ def plot_loss(path, epochs, train_loss, val_loss, angle, log=False):
         plt.yscale('log')
         fig.savefig(os.path.join(path, "TrainLoss_log.png"), format='png')
     plt.clf()
-   
+
+ 
 def extract_frames(video_path):
     """
     Extract the frames from a video by loading the video path.
@@ -111,104 +130,20 @@ def extract_frames(video_path):
     cap.release()
     return frames, fps
 
-# Using RT-Gene (CURRENTLY USED)  
-class extract_face_features():
+
+def normalized_cross_correlation(signal1, signal2):
     """
-    Class to extract relevant face features.
+    Perform cross correlation of two signals and normalized the output to [-1, 1].
     
-    Licensing:
-    Licensed under Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode)
+    Args:
+        signal1, signal2: numpy array of signals to cross correlate
+    Returns:
+        numpy array of normalized cross correlated signal, same length as input signals
     """
-    def __init__(self, device="cuda" if torch.cuda.is_available() else "cpu"):
-        self.device = device
-        script_path = os.path.dirname(os.path.realpath(__file__))
-        sys.path.append(os.path.abspath(os.path.join(script_path, '../..')))
-        self.landmark_estimator = LandmarkMethodBase(device_id_facedetection=self.device,
-                                            checkpoint_path_face=os.path.abspath(os.path.join(script_path, "../src/rtgene/model_nets/SFD/s3fd_facedetector.pth")),
-                                            checkpoint_path_landmark=os.path.abspath(
-                                                os.path.join(script_path, "../src/rtgene/model_nets/phase1_wpdc_vdc.pth.tar")),
-                                            model_points_file=os.path.abspath(os.path.join(script_path, "../src/rtgene/model_nets/face_model_68.txt")))
+    mean1 = signal1 - np.mean(signal1)
+    mean2 = signal2 - np.mean(signal2)
+    corr = np.correlate(mean1, mean2, mode='same')
+    norm_factor = np.std(signal1) * np.std(signal2) * len(signal1)
     
-    def __extract_eye_image_patches(self, subject):
-        le_c, re_c, _, _ = subject.get_eye_image_from_landmarks(subject, self.landmark_estimator.eye_image_size)
-        subject.left_eye_color = le_c
-        subject.right_eye_color = re_c
-
-
-    def extract_face_images(self, frames):
-        """
-        Extract face from video frames.
-        
-        Args:
-            frames: list of the video frames
-        Returns:
-            faces: list of face images extracted from each frame
-            subjects: extracted faces stored in list as objects for each frame
-        """
-        faces = []
-        subjects = []
-        for frame in frames:
-            faceboxes = self.landmark_estimator.get_face_bb(frame)
-            if len(faceboxes) == 0:
-                print('Could not find faces in the image')
-                return
-
-            img_subjects = self.landmark_estimator.get_subjects_from_faceboxes(frame, faceboxes)
-            for subject in img_subjects:
-                subjects.append(subject) # subject = object
-                # subject.face_color contains face image of shape (227, 227, 3)
-                faces.append(subject.face_color)
-                # cv2.imwrite('output_face.png', subject.face_color)
-            
-        return faces, subjects
-            
-    def extract_eye_images(self, frames):
-        """
-        Extract left and right eyes from video frames.
-        
-        Args:
-            frames: list of the video frames
-        Returns:
-            eyes_left: list of left eye images extracted from each frame
-            eyes_right: list of right eye images extracted from each frame
-
-        """
-        # Extract face images
-        faces, subjects = self.extract_face_images(frames)
-
-        eyes_left = []
-        eyes_right = []
-        # For each frame where face is extracted, extract left and right eyes
-        for subject in subjects:
-            # Extract eye patches
-            self.__extract_eye_image_patches(subject)
-            if subject.left_eye_color is None or subject.right_eye_color is None:
-                print('Failed to extract eye image patches')
-                continue
-            else:
-                eyes_left.append(torch.from_numpy(subject.left_eye_color).float())
-                eyes_right.append(torch.from_numpy(subject.right_eye_color).float())
-                # print(subject.left_eye_color.shape)
-                # cv2.imwrite('output_eye_left.png', subject.left_eye_color)
-                # print(subject.right_eye_color.shape)
-                # cv2.imwrite('output_eye_right.png', subject.right_eye_color)
-                
-        # Processing of eye images
-        eyes_left = np.array(eyes_left)
-        eyes_right = np.array(eyes_right)
-            
-        eyes_left = torch.from_numpy(eyes_left).float().cuda(self.device)
-        eyes_right = torch.from_numpy(eyes_right).float().cuda(self.device)
-        
-        eyes_left = eyes_left.permute(0, 3, 1, 2)
-        eyes_right = eyes_right.permute(0, 3, 1, 2)
-        
-        transformations = transforms.Compose([
-                transforms.Normalize(mean=[0.2323, 0.2088, 0.1767],
-                                     std=[0.0621, 0.0577, 0.0613])
-            ])
-        
-        eyes_left = transformations(eyes_left)
-        eyes_right = transformations(eyes_right)
-        
-        return eyes_left, eyes_right
+    # Return correlation (avoiding division by zero)
+    return corr / norm_factor if norm_factor != 0 else corr
