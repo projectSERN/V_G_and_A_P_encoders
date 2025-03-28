f3921109cc148081ac94c2d4ace1e3d21ece6be0
NuhaAz <nuhaaisyah9@gmail.com>
2025-03-28 12:15:02 +0000

    Update eye image extraction and error check

--- src/feature_detector.py
+++ src/feature_detector.py
@@ -119,6 +119,7 @@ class extract_face_features():
             out.write(frame)
         out.release()
     
+    # Ignore for gaze estimation
     def extract_subjects(self, frames):
         """
         Extract subjects from video frames.
@@ -187,64 +188,305 @@ class extract_face_features():
         
         return subjects, error
             
-    def extract_eye_images(self, subjects):
+            
+    def determine_errors(self, frames):
         """
-        Extract left and right eyes from video frames.
+        Determine the errors in the video frames.
         
         Args:
-            subjects: dictionary of subjects extracted from video frames
+            frames: list of video frames
         Returns:
-            eyes_left: dictionary of left eye images extracted from each subject for each frame
-            eyes_right: dictionary of right eye images extracted from each subject for each frame
-            error: error message if no eyes detected (default = None)
+            subjects: dictionary of extracted faces, {'subject index': [subject object for each frame]}
+            error: error message associated with the video (default = None)
         """
         error = None
+        subjects = {}
         eyes_left = {}
         eyes_right = {}
-        index = 0
-        # For each frame where face is extracted, extract left and right eyes
-        for index in subjects:
-            # Define subject in dictionary
-            eyes_left[index] = []
-            eyes_right[index] = []
+        previous_faceboxes = None
+        
+        current_frame = 0
+        count = 0 # Count number of frames subject not detected
+        for frame in frames:
+            faceboxes = self.landmark_estimator.get_face_bb(frame)
+            if len(faceboxes) == 0:
+                count += 1
+                if count > 1:
+                    # As human eyes move ~20-40ms, if face not detected for more than 1 continuous frame, ignore video
+                    print('ERROR: Could not find faces in the image')
+                    return [], "Face"
+                continue
+            else:
+                count = 0
             
-            for subject in subjects[index]:
-                # Go through each frame for each subject
+            current_subjects = self.landmark_estimator.get_subjects_from_faceboxes(frame, faceboxes)
+            
+            if previous_faceboxes is not None:
+                # Match faceboxes to previous faceboxes
+                matched_subjects = self.__match_faces(previous_faceboxes, current_subjects)
                 
-                # Extract eye patches
-                self.__extract_eye_image_patches(subject)
-                if subject.left_eye_color is None or subject.right_eye_color is None:
-                    # If no eyes detected, disregard video
-                    print('ERROR: Could not find eyes in the image')
-                    return None, None, "Eyes"
-                else:
-                    eyes_left[index].append(torch.from_numpy(subject.left_eye_color).float())
-                    eyes_right[index].append(torch.from_numpy(subject.right_eye_color).float())
+                # If previous subject not detected in current frame, append None to its current frame
+                for subject in subjects:
+                    if subject not in matched_subjects:
+                        matched_subjects[subject] = None       
+                        
+                for matched_subject in matched_subjects:
+                    if matched_subject not in subjects:
+                        # Add new dictionary key if new face detected
+                        new_subject = matched_subjects[matched_subject]
+                        new_subject_index = len(subjects)
+                        subjects[new_subject_index] = [None] * current_frame
+                        subjects[new_subject_index].append(new_subject)
+                        
+                        # Extract eye patches
+                        self.__extract_eye_image_patches(new_subject)
+                        
+                        eyes_left[new_subject_index] = [None] * current_frame
+                        eyes_right[new_subject_index] = [None] * current_frame
+                        if new_subject.left_eye_color is None or new_subject.right_eye_color is None:
+                            eyes_left[new_subject_index].append(None)
+                            eyes_right[new_subject_index].append(None)
+                        else:
+                            eyes_left[new_subject_index].append(torch.from_numpy(new_subject.left_eye_color).float())
+                            eyes_right[new_subject_index].append(torch.from_numpy(new_subject.right_eye_color).float())
+                    else:   
+                        # Append subject object depending on subject index (i.e. value of matched_subject)
+                        subject = matched_subjects[matched_subject]
+                        subjects[matched_subject].append(subject)
+                        
+                        if subject is not None:
+                            # Extract eye patches
+                            self.__extract_eye_image_patches(subject)
+                            if subject.left_eye_color is None or subject.right_eye_color is None:
+                                eyes_left[matched_subject].append(None)
+                                eyes_right[matched_subject].append(None)
+                            else:
+                                eyes_left[matched_subject].append(torch.from_numpy(subject.left_eye_color).float())
+                                eyes_right[matched_subject].append(torch.from_numpy(subject.right_eye_color).float())
+            else:
+                for index in range(len(current_subjects)):
+                    subject = current_subjects[index]
+                    subjects[index] = [subject]
                     
-            # Processing of eye images
-            eyes_left[index] = np.array(eyes_left[index])
-            eyes_right[index] = np.array(eyes_right[index])
-                
-            eyes_left[index] = torch.from_numpy(eyes_left[index]).float().cuda(self.device)
-            eyes_right[index] = torch.from_numpy(eyes_right[index]).float().cuda(self.device)
+                    # Extract eye patches
+                    self.__extract_eye_image_patches(subject)
+                    if subject.left_eye_color is None or subject.right_eye_color is None:
+                        eyes_left[index] = [None]
+                        eyes_right[index] = [None]
+                    else:
+                        eyes_left[index] = [torch.from_numpy(subject.left_eye_color).float()]
+                        eyes_right[index] = [torch.from_numpy(subject.right_eye_color).float()]
             
-            if eyes_left[index].dim() == 1 or eyes_right[index].dim() == 1:
+            # Link previous faceboxes based on subject index
+            previous_faceboxes = {}
+            for subject in subjects:
+                previous_faceboxes[subject] = subjects[subject][current_frame]
+            current_frame += 1
+        
+        # Remove any subject tracked for less than one second
+        removed_subjects = []
+        for subject in subjects:
+            # Check if faces are semi-tracked
+            count = 0 # Count number of frames subject is not detected
+            for frame in range(len(subjects[subject])):
+                if subjects[subject][frame] is None:
+                    count += 1
+                    if count > 1:
+                        removed_subjects.append(subject)
+                        break
+                else:
+                    count = 0
+        
+        # Delete subjects that were not fully tracked
+        for subject in removed_subjects:
+            del subjects[subject]
+        
+        if len(subjects) > 1:
+            print(f"ERROR: More than one subject detected - {len(subjects)}")
+            return subjects, "Subjects"
+        
+        # Processing of eye images for fully tracked face (single subject only)
+        for subject in subjects:
+            # Check if eye images are semi-tracked
+            count = 0 # Count number of frames eye is not detected
+            for frame in range(len(eyes_left[subject])):
+                if eyes_left[subject][frame] is None or eyes_right[subject][frame] is None:
+                    count += 1
+                    if count > 1:
+                        # As human eyes move ~20-40ms, if eye not detected for more than 1 continuous frame, ignore video
+                        print("ERROR: Failed to extract eye images")
+                        return [], "Eyes"
+                else:
+                    count = 0
+            
+            # Remove None values from eye images
+            eyes_left[subject] = [eye for eye in eyes_left[subject] if eye is not None]
+            eyes_right[subject] = [eye for eye in eyes_right[subject] if eye is not None]
+            
+            # Perform processing of eye images
+            eyes_left[subject] = np.array(eyes_left[subject])
+            eyes_right[subject] = np.array(eyes_right[subject])
+                
+            eyes_left[subject] = torch.from_numpy(eyes_left[subject]).float().to(self.device)
+            eyes_right[subject] = torch.from_numpy(eyes_right[subject]).float().to(self.device)
+            
+            if eyes_left[subject].dim() == 1 or eyes_right[subject].dim() == 1:
                 # Need to investigate why error occurs
                 print("ERROR: Eye images are not correct shape")
-                return None, None, "Dimensions"
+                return [], "Dimensions"
             
-            eyes_left[index] = eyes_left[index].permute(0, 3, 1, 2)
-            eyes_right[index] = eyes_right[index].permute(0, 3, 1, 2)
+            eyes_left[subject] = eyes_left[subject].permute(0, 3, 1, 2)
+            eyes_right[subject] = eyes_right[subject].permute(0, 3, 1, 2)
             
             transformations = transforms.Compose([
                     transforms.Normalize(mean=[0.2323, 0.2088, 0.1767],
                                         std=[0.0621, 0.0577, 0.0613])
                 ])
             
-            eyes_left[index] = transformations(eyes_left[index])
-            eyes_right[index] = transformations(eyes_right[index])
+            eyes_left[subject] = transformations(eyes_left[subject])
+            eyes_right[subject] = transformations(eyes_right[subject])
         
-        return eyes_left, eyes_right, error
+        return subjects, error
+    
+    
+    def extract_eye_images(self, frames):
+        """
+        Extract left and right eyes from video frames. Based on extract_subjects method.
+        Assumes errorneous videos have been removed.
+        Video segmentation performed after this method.
+        
+        Args:
+            frames: list of video frames
+        Returns:
+            eyes_left: list of left eye images extracted from the subject for each frame
+            eyes_right: list of right eye images extracted from the subject for each frame
+            flag: list of flags indicating if eye images are extracted correctly
+        """
+        subjects = {}
+        eyes_left = {}
+        eyes_right = {}
+        previous_faceboxes = None
+        
+        current_frame = 0
+        count = 0 # Count number of frames subject not detected
+        for frame in frames:
+            faceboxes = self.landmark_estimator.get_face_bb(frame)
+            if len(faceboxes) == 0:
+                count += 1
+                if count > 1:
+                    # As human eyes move ~20-40ms, if face not detected for more than 1 continuous frame, ignore video
+                    print('ERROR: Could not find faces in the image')
+                    return None, None, None
+                current_frame += 1
+                for subject in subjects:
+                    subjects[subject].append(None)
+                continue
+            else:
+                count = 0
+            
+            # Get subjects from faceboxes
+            current_subjects = self.landmark_estimator.get_subjects_from_faceboxes(frame, faceboxes)
+            
+            # Match faceboxes to subjects
+            if previous_faceboxes is not None:
+                # Match faceboxes to previous faceboxes
+                matched_subjects = self.__match_faces(previous_faceboxes, current_subjects)
+                
+                # If previous subject not detected in current frame, append None to its current frame
+                for subject in subjects:
+                    if subject not in matched_subjects:
+                        matched_subjects[subject] = None       
+                        
+                for matched_subject in matched_subjects:
+                    if matched_subject not in subjects:
+                        # Add new dictionary key if new face detected
+                        new_subject = matched_subjects[matched_subject]
+                        new_subject_index = len(subjects)
+                        subjects[new_subject_index] = [None] * current_frame
+                        subjects[new_subject_index].append(new_subject)
+                        
+                        # Extract eye patches
+                        self.__extract_eye_image_patches(new_subject)
+                        
+                        eyes_left[new_subject_index] = [None] * current_frame
+                        eyes_right[new_subject_index] = [None] * current_frame
+                        if new_subject.left_eye_color is None or new_subject.right_eye_color is None:
+                            eyes_left[new_subject_index].append(None)
+                            eyes_right[new_subject_index].append(None)
+                        else:
+                            eyes_left[new_subject_index].append(torch.from_numpy(new_subject.left_eye_color).float())
+                            eyes_right[new_subject_index].append(torch.from_numpy(new_subject.right_eye_color).float())
+                    else:   
+                        # Append subject object depending on subject index (i.e. value of matched_subject)
+                        subject = matched_subjects[matched_subject]
+                        subjects[matched_subject].append(subject)
+                        
+                        if subject is not None:
+                            # Extract eye patches
+                            self.__extract_eye_image_patches(subject)
+                            if subject.left_eye_color is None or subject.right_eye_color is None:
+                                eyes_left[matched_subject].append(None)
+                                eyes_right[matched_subject].append(None)
+                            else:
+                                eyes_left[matched_subject].append(torch.from_numpy(subject.left_eye_color).float())
+                                eyes_right[matched_subject].append(torch.from_numpy(subject.right_eye_color).float())
+            else:
+                for index in range(len(current_subjects)):
+                    subject = current_subjects[index]
+                    subjects[index] = [subject]
+                    
+                    # Extract eye patches
+                    self.__extract_eye_image_patches(subject)
+                    if subject.left_eye_color is None or subject.right_eye_color is None:
+                        eyes_left[index] = [None]
+                        eyes_right[index] = [None]
+                    else:
+                        eyes_left[index] = [torch.from_numpy(subject.left_eye_color).float()]
+                        eyes_right[index] = [torch.from_numpy(subject.right_eye_color).float()]
+            
+            # Link previous faceboxes based on subject index
+            previous_faceboxes = {}
+            for subject in subjects:
+                previous_faceboxes[subject] = subjects[subject][current_frame]
+            current_frame += 1
+        
+        # Remove any subject tracked for less than one second
+        removed_subjects = []
+        for subject in subjects:
+            # Check if faces are semi-tracked
+            count = 0 # Count number of frames subject is not detected
+            for frame in range(len(subjects[subject])):
+                if subjects[subject][frame] is None:
+                    count += 1
+                    if count > 1:
+                        removed_subjects.append(subject)
+                        break
+                else:
+                    count = 0
+        
+        # Delete subjects that were not fully tracked
+        for subject in removed_subjects:
+            del subjects[subject]
+            
+        if len(subjects) > 1:
+            print(f"ERROR: More than one subject detected - {len(subjects)}")
+            return None, None, None
+        else:
+            # Convert dictionary with single key to list
+            subject_idx = list(subjects.keys())[0]
+            eyes_left = eyes_left[subject_idx]
+            eyes_right = eyes_right[subject_idx]
+            
+            # Create flag list
+            flags = []
+            for index in range(len(eyes_left)):
+                if eyes_left[index] is None or eyes_right[index] is None:
+                    flags.append(False)
+                else:
+                    flags.append(True)
+         
+        return eyes_left, eyes_right, flags
+    
     
     def extract_lip_landmarks(self, subjects):
         """
