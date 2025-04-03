5dfbbd852ddd095a912370d05b505d9c6e6228df
NuhaAz <nuhaaisyah9@gmail.com>
2025-04-03 16:36:17 +0100

    Truncate gaze and pause to 290 data points

--- dataset.py
+++ dataset.py
@@ -1,10 +1,12 @@
 import os
+import glob
 import numpy as np
 from tqdm import tqdm
 
 import torch
 from torch.utils.data.dataset import Dataset
 import pandas as pd
+from scipy.signal import resample
 
 from src.gaze_detector import ARNetPipeline
 from src.pause_detector import PauseDetector
@@ -12,7 +14,7 @@ from src.feature_detector import extract_face_features
 from src.utils.dataset_MOD import select_frames, eye_image_transform, extract_frames, normalized_cross_correlation
 
 class DFDC_subset(Dataset):
-    def __init__(self, path, train=True, val=False, test=False,
+    def __init__(self, subset, train=True, val=False, test=False,
                  gaze_model='/scratch/zceenaa/ARE-Gaze_Model/edited/AREGaze-rtgene_best_model/best_model.pth',
                  device="cuda" if torch.cuda.is_available() else "cpu"):
         """
@@ -28,7 +30,8 @@ class DFDC_subset(Dataset):
             device: 'cuda' or 'cpu' (str)
         """
         self.device = device
-        self.path = path
+        self.subset = subset
+        self.path = f'/scratch/zceenaa/DFDC_subsets/subset_{self.subset}/'
         self.lines = []
         self.face_features_detector = extract_face_features(device=self.device)
         self.gaze_detector = ARNetPipeline(gaze_model, device=self.device)
@@ -77,8 +80,9 @@ class DFDC_subset(Dataset):
         # Get normalised cross correlation of yaw and pitch signals with pauses
         data = self._preprocess(video_path)
         
+        video_info = {'split': split, 'video': video}
         
-        return data, label, split
+        return data, label, video_info
     
     
     # Additional methods
@@ -96,13 +100,13 @@ class DFDC_subset(Dataset):
         """
         # Get frames from video
         frames, fps = extract_frames(video_path)
-        # fps = 30 # Hard code to avoid mathematical issues
         
         # Extract left and right eye images
         eyes_left, eyes_right, flags = self.face_features_detector.extract_eye_images(frames)
 
-        # Select frames with detected eyes
+        # Select frames with detected eyes (truncate to 290 frames)
         selected_frames = select_frames(flags, fps)
+        selected_frames = selected_frames[:290]
         
         selected_left = []
         selected_right = []
@@ -115,43 +119,62 @@ class DFDC_subset(Dataset):
         
         # Detect gaze
         gaze = self.gaze_detector.detect_gaze(selected_left, selected_right)
-        if gaze is None:
-            # Disregard video when gaze not detected
-            print("ERROR: Gaze not detected")
-            return "Gaze", video_path
         
-        # Detect pauses
-        _, pauses, _ = self.pause_detector.detect_pauses(video_path)
+        # Detect pauses (truncate to 220500 samples)
+        _, pauses, sr = self.pause_detector.detect_pauses(video_path)
+        pauses = pauses[:220500]
 
-        # Majority voting for pauses to relate to gaze
-        items = len(pauses)//len(gaze)
-        pauses_matched = [] # Pause frequency matched to gaze (i.e. per frame)
-        for index in range(0, len(pauses), items):
-            sample = pauses[index:index+items]
-            num_neg = np.sum(np.array(sample) == -1)
-            num_pos = np.sum(np.array(sample) == 1)
-            value = 1 if (num_pos > num_neg) else -1
-            pauses_matched.append(value)
+        # Downsample gaze to match fps of video (i.e. 29 fps)
+        pauses_matched = np.ravel(resample(pauses, 290))
             
-        yaw = gaze[:,0]
-        pitch = gaze[:,1]
+        yaw = np.ravel(gaze[:,0])
+        pitch = np.ravel(gaze[:,1])
         
         # Normalised cross correlation
         yaw_corr = normalized_cross_correlation(yaw, pauses_matched)
         pitch_corr = normalized_cross_correlation(pitch, pauses_matched)
 
-        data = {'yaw_corr': yaw_corr, 'pitch_corr': pitch_corr}
+        data = {'gaze': gaze, 'pauses': pauses, 'gaze-pause': [yaw_corr, pitch_corr]}
                 
         return data
+    
+    def save_preprocessed(self):
+        """
+        Save preprocessed data of DFDC dataset.
+        Information to save - gaze=[yaw,pitch], pauses=[tagged pauses], gaze_pause_corr=[yaw_corr,pitch_corr]
+        """
+        video_paths = []
+        data_split = []
+        gaze = []
+        pauses = []
+        corr = []
+        
+        print(f"Checking for errors in subset_{self.subset} -------device: {self.device}")
+        # for idx in tqdm(range(self.__len__())):
+        for idx in tqdm(range(10)):
+            data, label, video_info = self.__getitem__(idx)
+            split = video_info['split']
+            print(f'Data: {data}\nLabel: {label} Split: {split}')
+            
+            video_paths.append(video_info['video'])
+            data_split.append(video_info['split'])
+            gaze.append(data['gaze'])
+            pauses.append(data['pauses'])
+            corr.append(data['gaze-pause'])
+        
+        df = pd.DataFrame({
+            'video': [path for path in video_paths],
+            'split': [split for split in data_split],
+            'gaze': gaze,
+            'pauses': pauses,
+            'corr': corr
+        })
+
+        df.to_csv(f'/scratch/zceenaa/DFDC_subsets_preprocessed/subset_{self.subset}_data.csv', index=False)
+        
         
 
 if __name__ == "__main__":
-    path = '/scratch/zceenaa/DFDC_subsets/subset_01/'
-    save_path = '/scratch/zceenaa/DFDC_subsets_preprocessed/subset_09/'
-    save_path_2 = '/scratch/zceenaa/DFDC_test/'
-    preprocessed_path = '/scratch/zceenaa/DFDC_subsets_preprocessed/'       
-    
-    dataset = DFDC_subset(path)
-    data, label, split = dataset.__getitem__(0)
-    print(data)
-    print(len(data))
+    dataset = DFDC_subset(subset='01', device='cuda')
+    data, label, video_info = dataset.__getitem__(53)
+    # dataset.save_preprocessed
